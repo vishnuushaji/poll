84ce8a4c450c089a1ddec508a3281a221911d631
Vishnu <vishnushaji179@gmail.com>
2024-01-16 17:12:03 +0530

    Your commit message here

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,8 +114,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
+
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
