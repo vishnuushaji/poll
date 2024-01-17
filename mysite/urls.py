4ab8c99054de17ef7a87a9e7b89e9b30499a18bc
Vishnu <vishnushaji179@gmail.com>
2024-01-17 16:59:24 +0530

    first commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,4 +5,4 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("polls.urls")),
     path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
